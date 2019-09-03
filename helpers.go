4bdf86a0f7c62bd3401b453c1896ba1206736601
Artem Zakharov <azzzak@gmail.com>
2019-09-03 09:14:38 +0300

    add upload functions

--- helpers.go
+++ helpers.go
@@ -16,10 +16,10 @@ type AlbumUploadResp struct {
 	PhotosList string `json:"photos_list"`
 	Hash       string `json:"hash"`
 	AID        int    `json:"aid"`
-} //file1-5
+}
 
-// AlbumUploadFromReader загрузка фото в альбом из io.Reader.
-func AlbumUploadFromReader(uploadURL, filename string, reader io.Reader) (*AlbumUploadResp, error) {
+// AlbumUpload загрузка фото в альбом из io.Reader.
+func AlbumUpload(uploadURL, filename string, reader io.Reader) (*AlbumUploadResp, error) {
 	resp, err := uploadPhoto(uploadURL, filename, reader, new(AlbumUploadResp))
 	if err != nil {
 		return nil, err
@@ -34,7 +34,7 @@ func AlbumUploadFromFile(uploadURL, path string) (*AlbumUploadResp, error) {
 		return nil, err
 	}
 	defer file.Close()
-	return AlbumUploadFromReader(uploadURL, filepath.Base(path), file)
+	return AlbumUpload(uploadURL, filepath.Base(path), file)
 }
 
 // WallUploadResp фото на стене.
@@ -42,7 +42,26 @@ type WallUploadResp struct {
 	Server int    `json:"server"`
 	Photo  string `json:"photo"`
 	Hash   string `json:"hash"`
-} //photo
+}
+
+// WallUpload загрузка фото на стену из io.Reader.
+func WallUpload(uploadURL, filename string, reader io.Reader) (*WallUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(WallUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*WallUploadResp), nil
+}
+
+// WallUploadFromFile загрузка фото на стену из файла.
+func WallUploadFromFile(uploadURL, path string) (*WallUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return WallUpload(uploadURL, filepath.Base(path), file)
+}
 
 // AvatarUploadResp главное фото пользователя или сообщества.
 type AvatarUploadResp struct {
@@ -52,17 +71,36 @@ type AvatarUploadResp struct {
 	Hash        string `json:"hash"`
 	MessageCode int    `json:"message_code"`
 	ProfileAID  int    `json:"profile_aid"`
-} //photo
+}
+
+// AvatarUpload загрузка главного фото пользователя или сообщества из io.Reader.
+func AvatarUpload(uploadURL, filename string, reader io.Reader) (*AvatarUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(AvatarUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*AvatarUploadResp), nil
+}
+
+// AvatarUploadFromFile загрузка главного фото пользователя или сообщества из файла.
+func AvatarUploadFromFile(uploadURL, path string) (*AvatarUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return AvatarUpload(uploadURL, filepath.Base(path), file)
+}
 
 // MessageUploadResp фото в сообщении.
 type MessageUploadResp struct {
 	Server int    `json:"server"`
 	Photo  string `json:"photo"`
 	Hash   string `json:"hash"`
-} //photo
+}
 
-// MessageUploadFromReader загрузка фото в сообщение из io.Reader.
-func MessageUploadFromReader(uploadURL, filename string, reader io.Reader) (*MessageUploadResp, error) {
+// MessageUpload загрузка фото в сообщение из io.Reader.
+func MessageUpload(uploadURL, filename string, reader io.Reader) (*MessageUploadResp, error) {
 	resp, err := uploadPhoto(uploadURL, filename, reader, new(MessageUploadResp))
 	if err != nil {
 		return nil, err
@@ -77,13 +115,32 @@ func MessageUploadFromFile(uploadURL, path string) (*MessageUploadResp, error) {
 		return nil, err
 	}
 	defer file.Close()
-	return MessageUploadFromReader(uploadURL, filepath.Base(path), file)
+	return MessageUpload(uploadURL, filepath.Base(path), file)
 }
 
 // ChatCoverUploadResp главное фото для чата.
 type ChatCoverUploadResp struct {
 	Response int `json:"response"`
-} //file
+}
+
+// ChatUpload загрузка главного фото для чата из io.Reader.
+func ChatUpload(uploadURL, filename string, reader io.Reader) (*ChatCoverUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(ChatCoverUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*ChatCoverUploadResp), nil
+}
+
+// ChatUploadFromFile загрузка главного фото для чата из файла.
+func ChatUploadFromFile(uploadURL, path string) (*ChatCoverUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return ChatUpload(uploadURL, filepath.Base(path), file)
+}
 
 // ProductUploadResp фото товара.
 type ProductUploadResp struct {
@@ -92,39 +149,153 @@ type ProductUploadResp struct {
 	Hash     string `json:"hash"`
 	CropData string `json:"crop_data"`
 	CropHash string `json:"crop_hash"`
-} //file
+}
 
-// ProductSetUploadResp фото подборки товаров.
+// ProductUpload загрузка фото товара из io.Reader.
+func ProductUpload(uploadURL, filename string, reader io.Reader) (*ProductUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(ProductUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*ProductUploadResp), nil
+}
+
+// ProductUploadFromFile загрузка фото товара из файла.
+func ProductUploadFromFile(uploadURL, path string) (*ProductUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return ProductUpload(uploadURL, filepath.Base(path), file)
+}
+
+// ProductSetUploadResp фотоподборка товаров.
 type ProductSetUploadResp struct {
 	Server int    `json:"server"`
 	Photo  string `json:"photo"`
 	GID    int    `json:"gid"`
 	Hash   string `json:"hash"`
-} //file
+}
+
+// ProductSetUpload загрузка фотоподборки товаров из io.Reader.
+func ProductSetUpload(uploadURL, filename string, reader io.Reader) (*ProductSetUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(ProductSetUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*ProductSetUploadResp), nil
+}
+
+// ProductSetUploadFromFile загрузка фотоподборки товаров из файла.
+func ProductSetUploadFromFile(uploadURL, path string) (*ProductSetUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return ProductSetUpload(uploadURL, filepath.Base(path), file)
+}
 
 // AudioUploadResp аудиозапись.
 type AudioUploadResp struct {
 	Redirect string `json:"redirect"`
 	Server   int    `json:"server"`
 	Audio    string `json:"audio"`
-} //file
+}
+
+// AudioUpload загрузка аудиозаписи из io.Reader.
+func AudioUpload(uploadURL, filename string, reader io.Reader) (*AudioUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(AudioUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*AudioUploadResp), nil
+}
+
+// AudioUploadFromFile загрузка аудиозаписи из файла.
+func AudioUploadFromFile(uploadURL, path string) (*AudioUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return AudioUpload(uploadURL, filepath.Base(path), file)
+}
 
 // VideoUploadResp видеозапись.
 type VideoUploadResp struct {
 	Size    int `json:"size,omitempty"`
 	VideoID int `json:"video_id,omitempty"`
-} // video_file
+}
+
+// VideoUpload загрузка видеозаписи из io.Reader.
+func VideoUpload(uploadURL, filename string, reader io.Reader) (*VideoUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(VideoUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*VideoUploadResp), nil
+}
+
+// VideoUploadFromFile загрузка видеозаписи из файла.
+func VideoUploadFromFile(uploadURL, path string) (*VideoUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return VideoUpload(uploadURL, filepath.Base(path), file)
+}
 
 // DocUploadResp документ.
 type DocUploadResp struct {
 	File string `json:"file,omitempty"`
-} // file
+}
+
+// DocUpload загрузка документа из io.Reader.
+func DocUpload(uploadURL, filename string, reader io.Reader) (*DocUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(DocUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*DocUploadResp), nil
+}
+
+// DocUploadFromFile загрузка документа из файла.
+func DocUploadFromFile(uploadURL, path string) (*DocUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return DocUpload(uploadURL, filepath.Base(path), file)
+}
 
 // GroupCoverUploadResp обложка сообщества.
 type GroupCoverUploadResp struct {
 	Photo string `json:"photo"`
 	Hash  string `json:"hash"`
-} // photo
+}
+
+// GroupCoverUpload загрузка обложки сообщества из io.Reader.
+func GroupCoverUpload(uploadURL, filename string, reader io.Reader) (*GroupCoverUploadResp, error) {
+	resp, err := uploadPhoto(uploadURL, filename, reader, new(GroupCoverUploadResp))
+	if err != nil {
+		return nil, err
+	}
+	return resp.(*GroupCoverUploadResp), nil
+}
+
+// GroupCoverUploadFromFile загрузка обложки сообщества из файла.
+func GroupCoverUploadFromFile(uploadURL, path string) (*GroupCoverUploadResp, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return GroupCoverUpload(uploadURL, filepath.Base(path), file)
+}
 
 func uploadPhoto(uploadURL, filename string, reader io.Reader, holder interface{}) (interface{}, error) {
 	var fieldName string
